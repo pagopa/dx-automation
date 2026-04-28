d385cf54570e72ae0c7080aa8e31b7e9fdaba2a8
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2026-04-28 18:10:40 +0200

    Fix ad group names

--- infra/bootstrapper/uat/data.tf
+++ infra/bootstrapper/uat/data.tf
@@ -1,11 +1,11 @@
 data "azuread_group" "admins" {
-  display_name = "dx-u-auto-adgroup-admin"
+  display_name = "dx-u-adgroup-admin"
 }
 
 data "azuread_group" "developers" {
-  display_name = "dx-u-auto-adgroup-developers"
+  display_name = "dx-u-adgroup-developers"
 }
 
 data "azuread_group" "externals" {
-  display_name = "dx-u-auto-adgroup-externals"
+  display_name = "dx-u-adgroup-externals"
 }
